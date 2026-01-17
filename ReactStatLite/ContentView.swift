ee48d8996e7a5dcaf6b28d30dab7f6fc36256477
Rohan Khera <rohankhera2001@gmail.com>
2026-01-16 18:39:39 -0600

    Initial Commit

---
+++ ReactStatLite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReactStatLite
+//
+//  Created by Rohan Khera on 1/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReactStatLite/ReactStatLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactStatLiteApp.swift
+//  ReactStatLite
+//
+//  Created by Rohan Khera on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ReactStatLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
